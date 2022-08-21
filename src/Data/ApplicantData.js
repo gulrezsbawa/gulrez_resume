acb4e73270b5eb4e4a9ee77aedc2b89577aed02c
Gulrez Bawa <gulrezsbawa@gmail.com>
2022-08-21 14:07:20 +0530

    feat(gr): initial commit

--- src/Data/ApplicantData.js
+++ src/Data/ApplicantData.js
@@ -1,18 +1,18 @@
 export const applicantData = {
-  applicantName: "Kaveri",
-  designation: "Senior Business Associate",
-  briefSummary:
-    "Experienced and impactful Business Analyst with more than 6 years of experience working with a wide variety of platforms and clients.",
-  mail: "kaveri0491@gmail.com",
-  mobile: "+1-317-540-5175",
-  website: "kaveribawa.com",
-  linkedIn: "linkedin.com/in/kaveri-bawa",
+  applicantName: "Gulrez",
+  designation: "Senior Software Engineer",
+  briefSummary: `Full stack web developer, passion & expertise in process workflow automation. Love to
+    Code, Live to Code. Fast Learner. 2 Instant Puraskaar Award for performance
+    improvement. Handling Major Projects of Company with big responsibilities.`,
+  mail: "gulrez0491@gmail.com",
+  mobile: "+1-317-540-5173",
+  website: "gulrezbawa.com",
+  linkedIn: "linkedin.com/in/gulrez-bawa",
   certifications: {
     title: "CERTIFICATIONS",
     list: [
       {
-        subTitle:
-          "Certified HIPAA (Health Insurance Portability and Accountability Act)",
+        subTitle: "DevOps Certified",
         year: "2018",
         details: [],
       },
@@ -27,8 +27,8 @@ export const applicantData = {
         details: [],
       },
       {
-        subTitle: "Bachelor of Science in Information Technology (B.Sc. IT)",
-        year: "2013",
+        subTitle: "Bachelor of Computer Application (BCA)",
+        year: "2012",
         details: [],
       },
     ],
@@ -37,18 +37,36 @@ export const applicantData = {
     title: "HONORS & AWARDS",
     list: [
       {
-        subTitle: "Spot Puraskaar Award 2 Time by Director",
-        year: "2018",
+        subTitle: "Instant Puraskaar Award 2 Time by Assistant Vice President",
+        year: "2018 & 2019",
         details: [
-          "For outstanding performance and consistently resolving client’s issues within defined SLA",
-          "For successful release of HIX 8.6 and HIX 8.9, a next version of Health Insurance App, (for Hix Insurance Center) with extensive analysis & testing.",
+          `Improved in performance for issue ticketing application by
+          writing stimulation programs for sending/receiving 16K emails
+          to the application`,
+          `Developing Lazy Loading to improve performance`,
+          `Caching module to improve performance`,
+          `Creation of dynamic SQL E-Mail Template which is reused in all
+          applications`,
+          `Maintenance &amp; support for billing tool, handling process of
+          organization’s monthly billing.`,
+          `Training Management, Compliance Tool, Rest API`,
         ],
       },
     ],
   },
   expertise: {
     title: "EXPERTISE",
-    list: ["EDI ( ANSI X12 )", "HIPAA 837"],
+    list: [
+      "React JS",
+      "Typescript",
+      "Javascript",
+      "HTML",
+      "CSS",
+      "DHTMLX",
+      "Outlook Add-In",
+      "React Final Form",
+      "BaseWeb Design",
+    ],
   },
   skills: {
     title: "SKILLS",
@@ -57,7 +75,6 @@ export const applicantData = {
       "Problem solving",
       "Interpersonal",
       "Requirement Gathering & Analysis",
-      "Gap Analysis",
       "Planning / Prioritizing",
       "Decision Making",
       "Good communication",
@@ -73,52 +90,41 @@ export const applicantData = {
       "Excel",
       "Powerpoint",
       "Visio",
-      "Sharepoint",
-      "Provider Portal",
-      "Edifecs",
       "SQL",
-      "Health Insurance Exchange (HIX)",
-      "MapBuilder",
-      "Freshdesk",
+      "Postman",
+      "Visual Studio Code",
+      "Visual Studio Professional",
+      "Android Studio",
+      "Unity 3D",
+      "Figma",
     ],
   },
   professionServices: {
     title: "PROFESSIONAL EXPERIENCE",
     list: [
       {
-        name: "Infosys Limited",
-        year: "2018 - Present",
-        subTitle: "Senior Associate Consultant",
+        name: "Jifflenow Technology",
+        year: "2021 - Present",
+        subTitle: "Senior Software Engineer",
         details: [
-          `Owning and Operating the systems which ensures comprehensive
-             and high-quality data submissions to the states accordingly to
-             stringent data quality standards laid down by each state`,
-          `Extracting claims from database as 837 files and submitting them
-             to the state, ensuring successful file transmission`,
-          `Analyzing and processing the received response files from state
-             and resubmitting in case of rejected claims`,
+          `Work as front end developer`,
+          `Working on customer facing modules`,
+          `Expertise in Time zone handling using Luxon Library`,
+          `Creation of Scheduler using DHTMLX library`,
         ],
       },
       {
-        name: "Edifecs Technologies Private Limited",
-        year: "2016 - 2018",
-        subTitle: "Associate Product Support Specialist",
+        name: "Siemens Technology and Services Private Limited (STSPL)",
+        year: "2016 - 2021",
+        subTitle: "Senior Software Engineer",
         details: [
-          `Through HIX Solutions we helped Exchange (Center of Medicaid
-                    and Medicare) to maintain its data and helped in reconciliation
-                    data between Exchange and Payers (FCH/FLB/NC/IBC/VT).`,
-          `Successfully collaborating with the Business End-users, various
-                    stakeholders, technical experts and project management for
-                    efficient Solutions design, consequent development and process
-                    improvements.`,
-          `Good domain Knowledge of Membership/Enrollment, Benefits,
-                    Claims, Providers, Payers, Medicare and Medicaid`,
-          `Evaluating problems and helping different teams like
-                    Development, Customer Support, Documentation and ESD with
-                    their queries.`,
-          `Conducted workshops to gather high-level requirements with
-                    Business executives and estimated the product launch planning
-                    and document them as high-level stories in the project backlog`,
+          `Full stack web developer responsible for end-to-end web app
+          development.`,
+          `Prototyped new product features to improve productivity`,
+          `Handling organization’s Employee Database and processing as per
+          different tools`,
+          `Boosted user experience (UX/UI) scores over company-wide
+          previous best`,
         ],
       },
     ],
